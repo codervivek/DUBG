abbf7e88a72194d84ee34b28c25c2f6924699ca7
Pavan Kumar <pavan71198@users.noreply.github.com>
2018-10-24 14:59:27 +0530

    Added status of users

--- web/dashboard/models.py
+++ web/dashboard/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class Status(models.Model):
-    postedby = models.ForeignKey(User, on_delete=models.CASCADE)
+    postedby = models.ForeignKey(User, on_delete=models.CASCADE, help_text="a")
     latitude = models.DecimalField(max_digits=30, decimal_places=15)
     longitude = models.DecimalField(max_digits=30, decimal_places=15)
     num_people = models.PositiveIntegerField()
